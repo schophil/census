8a4eac33c6b944ce5472eb2fb1cd96b97536a64f
Philippe Schottey <lord_bytes@icloud.com>
2017-02-06 21:17:28 +0100

    about component

---
+++ client/src/app/about/about-component.js
@@ -0,0 +1,15 @@
+// vim: set sw=2 ts=2:
+
+(function () {
+
+	Vue.component('census-about', {
+		template: `
+			<census-panel>
+				<p>Census version 2.0.0 (snapshot)</p>
+				<p>Developed by Philippe Schottey</p>
+			</census-panel>
+		`,
+	});
+
+})();
+
