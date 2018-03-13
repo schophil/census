0e441c384c6eb5d64d6c578df69bafad1da4d41a
schophil <schophil@icloud.com>
2018-03-13 07:48:10 +0100

    Docker image with preloaded test data on a postgresql database

--- docker/censusdb/08_import.sql
+++ docker/censusdb/08_import.sql
@@ -1,7 +1,7 @@
-copy census_users from '/docker-entrypoint-initdb.d/data_users.csv' with delimiter ',' quote '"' csv;
-copy census_daystats from '/docker-entrypoint-initdb.d/data_daystats.csv' with delimiter ',' quote '"' csv;
-copy census_dayactivity from '/docker-entrypoint-initdb.d/data_dayactivity.csv' with delimiter ',' quote '"' csv;
-copy census_resource from '/docker-entrypoint-initdb.d/data_resource.csv' with delimiter ',' quote '"' csv;
-copy census_userstats from '/docker-entrypoint-initdb.d/data_userstats.csv' with delimiter ',' quote '"' csv;
-copy census_useractivity from '/docker-entrypoint-initdb.d/data_useractivity.csv' with delimiter ',' quote '"' csv;
-copy census_sourceip from '/docker-entrypoint-initdb.d/data_sourceip.csv' with delimiter ',' quote '"' csv;
+copy census_users from '/docker-entrypoint-initdb.d/data_users.csv' with (format csv, delimiter ';', quote '"');
+copy census_daystats from '/docker-entrypoint-initdb.d/data_daystats.csv' with (format csv, delimiter ';', quote '"');
+copy census_dayactivity from '/docker-entrypoint-initdb.d/data_dayactivity.csv' with (format csv, delimiter ';', quote '"');
+copy census_resource from '/docker-entrypoint-initdb.d/data_resource.csv' with (format csv, delimiter ';', quote '"');
+copy census_userstats from '/docker-entrypoint-initdb.d/data_userstats.csv' with (format csv, delimiter ';', quote '"');
+copy census_useractivity from '/docker-entrypoint-initdb.d/data_useractivity.csv' with (format csv, delimiter ';', quote '"');
+copy census_sourceip from '/docker-entrypoint-initdb.d/data_sourceip.csv' with (format csv, delimiter ';', quote '"');
