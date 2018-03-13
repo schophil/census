3dee45270c720c92cc03beeda781bf01e7eb939e
Philippe Schottey <schophil@icloud.com>
2018-03-13 20:39:41 +0100

    Utility launch scripts

--- dist.sh
+++ dist.sh
@@ -3,11 +3,12 @@
 # Step 1: build client
 cd ./client
 npm run clean
+npm install
 npm run dist
 cd ..
 
 # Step 2: copy client in server
-rm -R ./server/src/main/resources/static/*
+rm -rf ./server/src/main/resources/static/*
 cp -R ./client/dist/* ./server/src/main/resources/static
 
 # Step 3: package server
--- run-webdev.sh
+++ run-webdev.sh
@@ -3,6 +3,8 @@
 my_home=`echo $HOME`/.census/
 echo "Starting census on $my_home"
 
+my_dir=`pwd`
+
 versions=(0 0)
 idx=0
 while read -r line
@@ -15,4 +17,4 @@ current=${versions[0]}
 next=${versions[1]}
 
 jarName="./server/target/census-$next.jar"
-java -Xmx40m -jar $jarName --spring.profiles.active=local --census.data=file://$my_home --census.config=config.xml
+java -jar $jarName --spring.profiles.active=local --spring.resources.static-locations=file://$my_dir/client/dist --census.data=file://$my_home --census.config=config.xml
